5abc6286805b8e7a49052196c6f6f530c8c7d7cb
Johannestj <johannestjo@gmail.com>
2023-04-17 13:57:40 +0200

    la til go mod og litt annet

--- tcp_client.go
+++ tcp_client.go
@@ -4,6 +4,8 @@ import (
 	"net"
 	"log"
 	"os"
+
+	"github.com/Johannestj/is105sem03/mycrypt"
 )
 
 func main() {
@@ -14,15 +16,22 @@ func main() {
     
 	log.Println("os.Args[1] = ", os.Args[1])
 
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
+
  	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
 	} 
+
+	
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 }
